019ca00b3ddadd7cf6f34eb516f7f26badcad776
iket0512 <iket.ag24@gmail.com>
2017-02-04 03:59:42 +0530

    maps

--- app/src/main/java/com/phoenix/codeutsava/maa/helper/Urls.java
+++ app/src/main/java/com/phoenix/codeutsava/maa/helper/Urls.java
@@ -6,11 +6,12 @@ package com.phoenix.codeutsava.maa.helper;
 
 public class Urls {
 
-    public static final String BASE_URL = "http://192.168.43.19:8080/";
+    public static final String BASE_URL = "http://127.0.0.1:8000/";
     //public static final String REQUEST_SPLASH_SCREEN = "version/";
-    public static final String REQUEST_WELCOME_SCREEN = "url/";
+    public static final String REQUEST_WELCOME_SCREEN = "welcome";
     public static final String REQUEST_LOGIN_SCREEN = "login/";
     public static final String REQUEST_OTP_SCREEN = "otp/";
+    public static final String REQUEST_GEOTAGS="map/";
 
 
 
