dae4c23236d7e72606c224a42420321116fe0fe8
AmanAg513 <amanagrawal513@gmail.com>
2017-02-04 15:29:13 +0530

    commit done

--- app/src/main/java/com/phoenix/codeutsava/maa/helper/Urls.java
+++ app/src/main/java/com/phoenix/codeutsava/maa/helper/Urls.java
@@ -6,11 +6,11 @@ package com.phoenix.codeutsava.maa.helper;
 
 public class Urls {
 
-    public static final String BASE_URL = "http://192.168.43.19:8080/";
+    public static final String BASE_URL = "http://192.168.43.119:8080/";
     //public static final String REQUEST_SPLASH_SCREEN = "version/";
     public static final String REQUEST_WELCOME_SCREEN = "url/";
     public static final String REQUEST_LOGIN_SCREEN = "login/";
-    public static final String REQUEST_OTP_SCREEN = "otp/";
+    public static final String REQUEST_OTP_SCREEN = "verify/";
     public static final String REQUEST_PDF= "pdf/";
     public static final String REQUEST_GALLERY= "gallery/";
 
