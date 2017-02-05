52b2d0c980c6d0b63b440c2ac4c9fefc749bb006
iket0512 <iket.ag24@gmail.com>
2017-02-05 07:13:14 +0530

    pull

--- app/src/main/java/com/phoenix/codeutsava/maa/home/HomePage.java
+++ app/src/main/java/com/phoenix/codeutsava/maa/home/HomePage.java
@@ -18,6 +18,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.phoenix.codeutsava.maa.R;
+import com.phoenix.codeutsava.maa.discuss.view.Questions;
 import com.phoenix.codeutsava.maa.home_page_vaccines_1.view.HomeFragment;
 import com.phoenix.codeutsava.maa.further_reading.view.FurtherReadingFragment;
 import com.phoenix.codeutsava.maa.gallery.view.GalleryFragment;
@@ -35,7 +36,7 @@ public class HomePage extends AppCompatActivity
         MultiDex.install(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setFragment(new HomeFragment(),"Geo");
+        setFragment(new Questions(),"Geo");
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
